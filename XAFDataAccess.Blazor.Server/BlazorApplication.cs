909aadb8b7763e0896d7688f588407009b35b0ee
Fredrick Ochieng <fredrick_ochieng@outlook.com>
2023-02-20 12:10:55 +0300

    Add project files.

---
+++ XAFDataAccess.Blazor.Server/BlazorApplication.cs
@@ -0,0 +1,49 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Blazor;
+using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.Security.ClientServer;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Xpo;
+using XAFDataAccess.Blazor.Server.Services;
+
+namespace XAFDataAccess.Blazor.Server;
+
+public class XAFDataAccessBlazorApplication : BlazorApplication {
+    public XAFDataAccessBlazorApplication() {
+        ApplicationName = "XAFDataAccess";
+        CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+        DatabaseVersionMismatch += XAFDataAccessBlazorApplication_DatabaseVersionMismatch;
+    }
+    protected override void OnSetupStarted() {
+        base.OnSetupStarted();
+#if DEBUG
+        if(System.Diagnostics.Debugger.IsAttached && CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
+            DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
+        }
+#endif
+    }
+    private void XAFDataAccessBlazorApplication_DatabaseVersionMismatch(object sender, DatabaseVersionMismatchEventArgs e) {
+#if EASYTEST
+        e.Updater.Update();
+        e.Handled = true;
+#else
+        if(System.Diagnostics.Debugger.IsAttached) {
+            e.Updater.Update();
+            e.Handled = true;
+        }
+        else {
+            string message = "The application cannot connect to the specified database, " +
+                "because the database doesn't exist, its version is older " +
+                "than that of the application or its schema does not match " +
+                "the ORM data model structure. To avoid this error, use one " +
+                "of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+            if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+                message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+            }
+            throw new InvalidOperationException(message);
+        }
+#endif
+    }
+}
---
+++ XAFDataAccess.Module/BusinessObjects/Customer.cs
@@ -0,0 +1,81 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XAFDataAccess.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class Customer : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        // Use CodeRush to create XPO classes and properties with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/118557
+        public Customer(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+
+
+        bool active;
+        decimal salary;
+        DateTime dateOfBirth;
+        string address;
+        string name;
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Name
+        {
+            get => name;
+            set => SetPropertyValue(nameof(Name), ref name, value);
+        }
+
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Address
+        {
+            get => address;
+            set => SetPropertyValue(nameof(Address), ref address, value);
+        }
+
+        
+        public bool Active
+        {
+            get => active;
+            set => SetPropertyValue(nameof(Active), ref active, value);
+        }
+
+
+        public DateTime DateOfBirth
+        {
+            get => dateOfBirth;
+            set => SetPropertyValue(nameof(DateOfBirth), ref dateOfBirth, value);
+        }
+
+        
+        public decimal Salary
+        {
+            get => salary;
+            set => SetPropertyValue(nameof(Salary), ref salary, value);
+        }
+    }
+
+    }
